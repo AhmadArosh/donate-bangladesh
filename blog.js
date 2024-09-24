b4e902a33467fe80f2118acc880656f2320b01cd
Ahmad Arosh <aarenbd@gmail.com>
2024-09-24 12:28:19 +0600

    first commit

---
+++ blog.js
@@ -0,0 +1,4 @@
+const blogBtn = document.getElementById("homeBtn");
+    homeBtn.addEventListener("click", function() {
+        window.location.href = "index.html";
+    });
---
+++ index.js
@@ -0,0 +1,6 @@
+ const blogBtn = document.getElementById("blogBtn");
+    blogBtn.addEventListener("click", function() {
+        window.location.href = "blog.html";
+    });
+
+    const navbar = document.getElementById("navbar")
