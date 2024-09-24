43b7caf0b157b87711b58019b730fd9edcf7b018
Ahmad Arosh <aarenbd@gmail.com>
2024-09-24 21:08:03 +0600

    added functionting buttons

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 //  navigating through pages //
 const blogBtn = document.getElementById("blogBtn");
 blogBtn.addEventListener("click", function() {
@@ -17,30 +18,73 @@ if (window.pageYOffset > sticky) {
     navbar.style.position = "";       
 }
 };
+
+// History Button //
+const page1 = document.getElementById('main')
+const button1 = document.getElementById('donate-btn');
+const button2 = document.getElementById('history-btn');
+button1.addEventListener('click', function(){
+    button1.style.backgroundColor = 'green';
+    button2.style.backgroundColor = 'white';
+    page1.style.display = 'block';
+    })
+    button2.addEventListener('click', function(){
+    button2.style.backgroundColor = 'green';
+    button1.style.backgroundColor = 'white';
+    page1.style.display = 'none';
+    })
 // Noakhali Donations//
+
 document.getElementById("add-money-btn")
+
 .addEventListener('click' , function(event){
+
 event.preventDefault();
+
 const addMoney = document.getElementById('donation-input').value ;
+
 const addMoneyNumber = parseFloat(addMoney)
+
 const balance = document.getElementById('noakhali-donation').innerText ;
+
 const balanceNumber = parseFloat(balance)
+
 const NewBalance = addMoneyNumber + balanceNumber ;
+
 document.getElementById('noakhali-donation').innerText = NewBalance
+
 console.log(NewBalance)
+
 //  Overall Balance //
 
+
+
 const overallBalance = document.getElementById('overall-balance').innerText ;
+
 const overallBalanceNumber = parseFloat(overallBalance)
+
 const NewoverallBalanceNumber = overallBalanceNumber - addMoneyNumber ;
+
 document.getElementById('overall-balance').innerText = NewoverallBalanceNumber ;
 
+
+
 // Debugs //
-if (NewoverallBalanceNumber < 0 || isNaN(addMoneyNumber) || addMoneyNumber < 0){
-alert('Insufficient Balance or Invalid Input')
-event.preventDefault()
+
+if (NewoverallBalanceNumber < 0 || isNan(addMoneyNumber) || addMoneyNumber
+
+< 0){
+    alert('Insufficient Balance or Invalid Input')
+    event.preventDefault()
 }
+
+else {
+
+   
+}
+
 });
+
 
 
 // Feni Donations//
@@ -88,11 +132,11 @@ document.getElementById('overall-balance').innerText = NewoverallBalanceNumber ;
 // Debugs //
 if (NewoverallBalanceNumber < 0 || isNaN(addMoneyNumber) || addMoneyNumber < 0){
 alert('Insufficient Balance or Invalid Input')
-event.preventDefault()
+
 }
 });
 // Notification //
-const donateButton = document.getElementById('add-money-btn');
+const donateButton = document.getElementById('add-money-btn')
 const popup = document.getElementById('pop-up');
 
 donateButton.addEventListener('click', () => {
@@ -103,3 +147,33 @@ const closeButton = document.getElementById('close-btn');
 closeButton.addEventListener('click', () => {
   popup.style.display = 'none';
 });
+
+// Notification 2 //
+const donateButton2 = document.getElementById('add-money-btn-2')
+const popup2 = document.getElementById('pop-up-2');
+
+donateButton2.addEventListener('click', () => {
+  popup2.style.display = 'block';
+});
+const closeButton2 = document.getElementById('close-btn-2');
+
+closeButton2.addEventListener('click', () => {
+  popup2.style.display = 'none';
+});
+
+// Notification 3 //
+const donateButton3 = document.getElementById('add-money-btn-3')
+const popup3 = document.getElementById('pop-up-3');
+
+donateButton3.addEventListener('click', () => {
+  popup3.style.display = 'block';
+});
+const closeButton3 = document.getElementById('close-btn-3');
+
+closeButton3.addEventListener('click', () => {
+  popup3.style.display = 'none';
+});
+
+// History of Transactions//
+
+
